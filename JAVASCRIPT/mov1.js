042a81f180b16b1788530c0f5cedf56af9c0289e
art751 <attaphon751@gmail.com>
2021-10-01 20:10:15 +0700

    sd

--- JAVASCRIPT/mov1.js
+++ JAVASCRIPT/mov1.js
@@ -1,16 +1,16 @@
-const usersList = document.querySelector(".container2 .mov2-list");
-setInterval(() =>{
-      //let's start Ajax
-    let xhr = new XMLHttpRequest();//creating XML object
-    xhr.open("GET", "php/mov1.php", true);
-    xhr.onload = ()=>{
-      if(xhr.readyState === XMLHttpRequest.DONE){
-          if(xhr.status === 200){
-            let data = xhr.response;   
-            usersList.innerHTML = data;
+// const usersList = document.querySelector(".container2 .mov2-list");
+// setInterval(() =>{
+//       //let's start Ajax
+//     let xhr = new XMLHttpRequest();//creating XML object
+//     xhr.open("GET", "php/mov1.php", true);
+//     xhr.onload = ()=>{
+//       if(xhr.readyState === XMLHttpRequest.DONE){
+//           if(xhr.status === 200){
+//             let data = xhr.response;   
+//             usersList.innerHTML = data;
 
-          }
-      }
-    }
-    xhr.send();
-  }, 500);//this function will run frequently after 500ms
+//           }
+//       }
+//     }
+//     xhr.send();
+//   }, 500);//this function will run frequently after 500ms
